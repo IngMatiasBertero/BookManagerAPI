44e81f716101b156bcc8c4db62cd3f98db900167
Matias Bertero <berteromatias99@gmail.com>
2023-12-04 20:53:17 -0300

    Proyecto Terminado

--- FirstAPI/Controllers/ControlLibros.cs
+++ FirstAPI/Controllers/ControlLibros.cs
@@ -25,17 +25,28 @@ namespace FirstAPI.Controllers
         }
 
         //GET: api/libros/2
-        [HttpGet("{id")]
+        [HttpGet("{id}")]
         public async Task<ActionResult<Libro>> GetLibro(int id)
         {
+            var libro = await _context.Libros.FindAsync(id);
 
+            if (libro == null)
+            {
+                return NotFound();
+            }
+
+            return libro;
         }
 
         //POST: api/libros
         [HttpPost]
         public async Task<ActionResult<Libro>> PostLibro(Libro libro)
         {
+            _context.Libros.Add(libro);
 
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetBook", new { id = libro.Id }, libro);
 
         }
 
@@ -44,12 +55,48 @@ namespace FirstAPI.Controllers
         public async Task<IActionResult> PutLibro(int id, Libro libro)
         {
 
-        }
+            if (id != libro.Id)
+            {
+                return BadRequest();
+
+            }
+
+            var libroInDb = await _context.Libros.FindAsync(id);
+
+            if (libroInDb == null)
+            {
+                return NotFound();
+            }
+
+            libroInDb.Titulo = libro.Titulo;
+            libroInDb.Autor = libro.Autor;
+            libroInDb.Habilitado = libro.Habilitado;
+
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+
+
+        }   
 
         //DELETE: api/libros/2
         [HttpDelete("{id}")]
         public async Task<ActionResult<Libro>> DeleteLibro(int id)
         {
+
+            var libro = await _context.Libros.FindAsync(id);
+
+            if (libro == null)
+            {
+                return NotFound();
+            }
+
+
+            _context.Libros.Remove(libro);
+            await _context.SaveChangesAsync();
+
+            return libro;
+
 
         }
 

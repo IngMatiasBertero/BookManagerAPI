22e7e9885688fb1a74dd2709226c66b505e4be8a
Matias Bertero <berteromatias99@gmail.com>
2023-12-04 17:05:49 -0300

    Esqueleto de la API

---
+++ FirstAPI/Controllers/ControlLibros.cs
@@ -0,0 +1,55 @@
+﻿using FirstAPI.Modelos;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FirstAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+
+    public class ControlLibros
+    {
+
+        public ControlLibros()
+        {
+
+        }
+
+        //GET: api/libros
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Libro>>> GetLibros()
+        {
+
+        }
+
+
+        //GET: api/libros/2
+        [HttpGet("{id")]
+        public async Task<ActionResult<Libro>> GetLibro(int id)
+        {
+
+        }
+
+        //POST: api/libros
+        [HttpPost]
+        public async Task<ActionResult<Libro>> PostLibro(Libro libro)
+        {
+
+
+        }
+
+        //PUT: api/libros/2
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutLibro(int id, Libro libro)
+        {
+
+        }
+
+        //DELETE: api/libros/2
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Libro>> DeleteLibro(int id)
+        {
+
+        }
+
+    }
+}
--- FirstAPI/Modelos/Libro.cs
+++ FirstAPI/Modelos/Libro.cs
@@ -2,5 +2,10 @@
 {
     public class Libro
     {
+        public int Id { get; set; }
+        public string Titulo { get; set; }
+        public string Autor { get; set; }
+        public bool Habilitado { get; set; }
+
     }
 }
